e9030241f45b137a05c31a3bf382ce4f7da401e6
shrizan <46957245+shrizan@users.noreply.github.com>
2022-12-09 21:37:50 +0545

    classes

--- 5.classes/8.py
+++ 5.classes/8.py
@@ -1,23 +1,23 @@
 class TagCloud:
     def __init__(self) -> None:
-        self.tags = {}
+        self.__tags = {}
 
     def add(self, tag):
-        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0)+1
+        self.__tags[tag.lower()] = self.__tags.get(tag.lower(), 0)+1
 
     # eg: tag_cloud["Python"]
     def __getitem__(self, tag):
-        return self.tags.get(tag.lower(), 0)
+        return self.__tags.get(tag.lower(), 0)
 
     # eg: tag_cloud["python"]=4
     def __setitem__(self, tag, count):
-        self.tags[tag.lower()] = count
+        self.__tags[tag.lower()] = count
 
     # eg: len(tag_cloud)
     def __len__(self):
-        return len(self.tags)
+        return len(self.__tags)
     # eg: for item in tag_cloud:
     #  print(item)
 
     def __iter__(self):
-        return iter(self.tags)
+        return iter(self.__tags)
