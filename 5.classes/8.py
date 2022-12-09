aee7591a14bf77fd055f295b3cc63e5e35820295
shrizan <46957245+shrizan@users.noreply.github.com>
2022-12-09 21:34:37 +0545

    classes

--- 5.classes/8.py
+++ 5.classes/8.py
@@ -21,11 +21,3 @@ class TagCloud:
 
     def __iter__(self):
         return iter(self.tags)
-
-
-cloud = TagCloud()
-cloud.add("Python")
-cloud.add("Python")
-cloud.add("Python")
-
-print(cloud["Python"])
